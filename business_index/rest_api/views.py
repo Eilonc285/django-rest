386cedf0b3f87bca73903e05a706a5e3198d6f90
Eilonc285 <50795839+Eilonc285@users.noreply.github.com>
2021-07-11 15:48:59 +0300

    Refactored BusinessView.get code into smaller functions.

--- business_index/rest_api/views.py
+++ business_index/rest_api/views.py
@@ -9,33 +9,23 @@ from .easy_web_scraper import get_first_business_info
 # Create your views here.
 
 class BusinessView(APIView):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.business_title = None
+        self.business_id = None
+        self.business = None
+        self.serialized_business_objects = None
+        self.final_response = status.HTTP_500_INTERNAL_SERVER_ERROR
+
     def get(self, request, *args, **kwargs):
         params = dict(request.query_params.items())
         if len(params) == 2:
-            try:
-                business_id = params['id']
-                business_title = params['title']
-            except KeyError as e:
-                print(e)
-                return Response(status=status.HTTP_400_BAD_REQUEST)
-            try:
-                serializer = BusinessSerializer(Business.objects.get(id=business_id, title=business_title))
-            except Business.DoesNotExist as e:
-                print(e)
-                return Response(status=status.HTTP_404_NOT_FOUND)
-            except ValueError as e:
-                print(e)
-                return Response(status=status.HTTP_400_BAD_REQUEST)
+            return self.create_response_for_request_of_single_business(params)
         elif len(params) == 0:
-            serializer = BusinessSerializer(Business.objects.all(), many=True)
+            return self.create_response_for_request_of_all_businesses(params)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        try:
-            return Response(serializer.data)
-        except Exception as e:
-            print(e)
-            print('Serializer may not be updated to model changes.')
-            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)  # never reached
 
     def post(self, request, *args, **kwargs):
         serializer = BusinessSerializer(data=request.data)
@@ -43,6 +33,56 @@ class BusinessView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+
+    def create_response_for_request_of_single_business(self, params):
+        try:
+            self.assign_id_and_title_params_to_instance_attributes(params)
+            self.make_query_for_matching_business_object_from_db()
+            self.serialize_queried_objects()
+            self.final_response = Response(self.serialized_business_objects)
+        except Exception as e:
+            pass  # all exceptions set a proper final response before raising exception.
+        return self.final_response
+
+    def assign_id_and_title_params_to_instance_attributes(self, params):
+        try:
+            self.business_id = params['id']
+            self.business_title = params['title']
+        except KeyError as e:
+            print(e)
+            self.final_response = Response(status=status.HTTP_400_BAD_REQUEST)
+            raise e
+
+    def make_query_for_matching_business_object_from_db(self):
+        try:
+            self.business = Business.objects.get(id=self.business_id, title=self.business_title)
+        except ValueError as e:
+            print(e)
+            self.final_response = Response(status=status.HTTP_400_BAD_REQUEST)
+            raise e
+        except Business.DoesNotExist as e:
+            print(e)
+            self.final_response = Response(status=status.HTTP_404_NOT_FOUND)
+            raise e
+
+    def serialize_queried_objects(self, many_objects=False):
+        serializer = BusinessSerializer(self.business, many=many_objects)
+        try:
+            self.serialized_business_objects = serializer.data
+        except Exception as e:
+            print(e)
+            print('Serializer may not be updated to model changes.')
+            self.final_response = Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            raise e
+
+    def create_response_for_request_of_all_businesses(self, params):
+        try:
+            self.business = Business.objects.all()
+            self.serialize_queried_objects(many_objects=True)
+            self.final_response = Response(self.serialized_business_objects)
+        except Exception as e:
+            pass
+        return self.final_response
 
 
 class WebScraperView(APIView):
